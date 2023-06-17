37467dbd0926c66be0f01597dbd8b8b12df0648d
riodel27 <riopamaong@gmail.com>
2023-06-18 00:21:28 +0800

    fix: Resolve type error on homepage by adding User interface
    This commit addresses the following issues:
    - Fixed a type error in the homepage component that occurred during the Vercel build deployment.
    - Added a User interface to ensure type safety and proper data structure for user-related data.

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,12 +1,13 @@
 import prisma from '@/lib/prisma';
 import { timeAgo } from '@/lib/utils';
+import { User } from '@/types';
 
 export default async function Home() {
   const users = await prisma.users.findMany();
 
   return (
     <main className='flex min-h-screen flex-col items-center justify-between p-24'>
-      {users.map((user) => (
+      {users.map((user: User) => (
         <div key={user.name} className='flex items-center justify-between py-3'>
           <div className='flex items-center space-x-4'>
             <div className='space-y-1'>
